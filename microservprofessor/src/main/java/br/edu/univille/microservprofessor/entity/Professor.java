6ad1b11bde6090d875654578d135744c9a582b39
Leonardo Zwierzykowski <79949114+leonardozw@users.noreply.github.com>
2023-11-09 20:28:37 +0000

    feat: validation added

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/entity/Professor.java
@@ -1,11 +1,17 @@
 package br.edu.univille.microservprofessor.entity;
 
+import java.time.Instant;
+import java.time.LocalDate;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import com.azure.spring.data.cosmos.core.mapping.Container;
 import com.azure.spring.data.cosmos.core.mapping.GeneratedValue;
 import com.azure.spring.data.cosmos.core.mapping.PartitionKey;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,11 +26,27 @@ public class Professor {
     @GeneratedValue
     private String id;
 
+    @NotBlank(message = "O nome é obrigatório.")
     private String nome;
 
     @PartitionKey
+    @NotBlank(message = "O documento é obrigatório.")
     private String documento;
 
-    private String endereco;
-    private Integer idade;
+    @NotBlank(message = "O cep é obrigatório.")
+    private String cep;
+
+    @NotBlank(message = "O telefone é obrigatório.")
+    private String telefone;
+
+    @NotBlank(message = "O email é obrigatório.")
+    private String email;
+
+    @NotNull(message = "A data de nascimento é obrigatória.")
+    private LocalDate dataNascimento;
+
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant LastUpdatedAt;
 }
