ed6da603cdbd86361cbe29dac6098deb34950ca4
Leonardo Zwierzykowski <79949114+leonardozw@users.noreply.github.com>
2023-11-09 20:29:35 +0000

    feat: changed to cosmosRepository and added other custom methods

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/repository/ProfessorRepository.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/repository/ProfessorRepository.java
@@ -3,12 +3,15 @@ package br.edu.univille.microservprofessor.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import com.azure.spring.data.cosmos.repository.CosmosRepository;
 
 import br.edu.univille.microservprofessor.entity.Professor;
 
-public interface ProfessorRepository extends CrudRepository<Professor, String>{
+public interface ProfessorRepository extends CosmosRepository<Professor, String>{
 
     List<Professor> findAll();
+    Optional<Professor> findById(String id);
     Optional<Professor> findByDocumento(String documento);
+    boolean existsByDocumento(String documento);
+    void deleteByDocumento(String documento, String key);
 }
