f4a274ce70f1d8ff55be66049fbbb0b0473b8024
Leonardo Zwierzykowski <79949114+leonardozw@users.noreply.github.com>
2023-11-16 20:12:23 +0000

    feat: removed redundant validation

--- microservprofessor/src/main/java/br/edu/univille/microservprofessor/controller/ProfessorUpdateRequest.java
+++ microservprofessor/src/main/java/br/edu/univille/microservprofessor/controller/ProfessorUpdateRequest.java
@@ -2,22 +2,10 @@ package br.edu.univille.microservprofessor.controller;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
 public record ProfessorUpdateRequest(
-    @NotBlank(message = "O nome é obrigatório.")
     String nome,
-
-    @NotBlank(message = "O cep é obrigatório.")
     String cep,
-
-    @NotBlank(message = "O telefone é obrigatório.")
     String telefone,
-
-    @NotBlank(message = "O email é obrigatório.")
     String email,
-
-    @NotNull(message = "A data de nascimento é obrigatória.")
     LocalDate dataNascimento
 ) {}
